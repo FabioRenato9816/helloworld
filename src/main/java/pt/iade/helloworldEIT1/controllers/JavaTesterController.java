0405c36f7d6d4d7729ac98d818762432cd7985a0
Fábio Renato Fernandes da Costa <fabiorenato393@gmail.com>
2020-11-14 15:39:39 +0000

    versão do java actualizada

---
+++ src/main/java/pt/iade/helloworldEIT1/controllers/JavaTesterController.java
@@ -0,0 +1,30 @@
+package pt.iade.helloworldEIT1.controllers;
+
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import pt.iade.helloworldEIT1.models.CurricularUnit;
+
+
+@RestController
+@RequestMapping(path="/api/java/tester")
+
+public class JavaTesterController {
+    private Logger logger = LoggerFactory.getLogger(GreeterController.class);  //como se fosse o System.out mas é bem formatado
+    private ArrayList<CurricularUnit> units = new ArrayList<CurricularUnit>();
+
+    @PostMapping(path = "/units", produces= MediaType.APPLICATION_JSON_VALUE)
+    public CurricularUnit saveUnit(@RequestBody CurricularUnit unit) {
+       logger.info("Added unit "+unit.getName());
+    units.add(unit);
+       return unit;
+    }
+    
+}
